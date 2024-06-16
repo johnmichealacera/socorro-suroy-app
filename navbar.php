3fb4ee70c43ced6663c1b22e15799bdfdb1a9617
JM Acera <john.acera@sportality.se>
2024-06-16 18:12:01 +0800

    finalized index.php, delete unused images and code cleanup

--- navbar.php
+++ navbar.php
@@ -13,17 +13,6 @@
                 <a href="tourist-spots.php" class="nav-item nav-link <?php echo ($pageTitle === 'tourist-spots') ? 'active' : ''; ?>">Spots</a>
                 <a href="delicacies.php" class="nav-item nav-link <?php echo ($pageTitle === 'delicacies') ? 'active' : ''; ?>">Delicacies</a>
                 <a href="barangays.php" class="nav-item nav-link <?php echo ($pageTitle === 'barangays') ? 'active' : ''; ?>">Barangays</a>
-                <!-- <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu border-0 rounded-0 m-0">
-                        <a href="single.php" class="dropdown-item <?php echo ($pageTitle === 'single') ? 'active' : ''; ?>">Blog Detail</a>
-                        <a href="destination.php" class="dropdown-item <?php echo ($pageTitle === 'destination') ? 'active' : ''; ?>">Destination</a>
-                        <a href="guide.php" class="dropdown-item <?php echo ($pageTitle === 'guide') ? 'active' : ''; ?>">Travel Guides</a>
-                        <a href="testimonial.php" class="dropdown-item <?php echo ($pageTitle === 'testimonial') ? 'active' : ''; ?>">Testimonial</a>
-                    </div>
-                </div> -->
-                <!-- <a href="about.php" class="nav-item nav-link <?php echo ($pageTitle === 'about') ? 'active' : ''; ?>">About</a> -->
-                <a href="contact-us.php" class="nav-item nav-link <?php echo ($pageTitle === 'contact') ? 'active' : ''; ?>">Contact</a>
               </div>
           </div>
       </nav>
